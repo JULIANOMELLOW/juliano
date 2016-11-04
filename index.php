55bb686313166cf094d8f663e130f5162f4fbbb0
EIIFRO - Ji-Paraná <eiifro@ifro.local>
2016-11-04 10:57:32 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+$ git add *
+$git commit -a -m "Initial commit"
+composer.json
+
+{}
+
+-----------------------------------
+index.php
+
+<?php
+
+echo "Olá cloud";
+
+-----------------------------------
+
+Initial commit
